133e74fff3b8b7c3012f6afa3bd2c244d1cb8b24
Tetsuya Mori <monry84@gmail.com>
2020-01-28 19:23:21 +0900

    :sparkles: Fix extension methods to bind SignalHandler and declare Signal
    * Signal の宣言と Handler の Bind を同時に行う拡張メソッドを DiContainer に生やす
    * 利用側は `Container.DeclareSignalWithHandler<TSignal>();` を叩くだけで OK

--- Assets/Scripts/Application/Extension/DiContainerExtension.cs
+++ Assets/Scripts/Application/Extension/DiContainerExtension.cs
@@ -1,11 +1,30 @@
+using JetBrains.Annotations;
 using Zenject;
 
 namespace SignalHandler.Application.Extension
 {
+    [PublicAPI]
     public static class DiContainerExtension
     {
-        public static DeclareSignalIdRequireHandlerAsyncTickPriorityCopyBinder ProclaimSignal<TSignal>(this DiContainer container) where TSignal : ISignal
+        public static DeclareSignalIdRequireHandlerAsyncTickPriorityCopyBinder DeclareSignalWithHandler<TSignal>(this DiContainer container) where TSignal : ISignal
         {
+            if (!container.HasBinding<SignalBus>())
+            {
+                container.Bind<SignalBus>().AsCached();
+            }
+
+            container.BindInterfacesTo<SignalHandler<TSignal>>().AsCached();
+            return container.DeclareSignal<TSignal>();
+        }
+
+        public static DeclareSignalIdRequireHandlerAsyncTickPriorityCopyBinder DeclareSignalWithHandler<TSignal, TParameter>(this DiContainer container) where TSignal : ISignal<TParameter>
+        {
+            if (!container.HasBinding<SignalBus>())
+            {
+                container.Bind<SignalBus>().AsCached();
+            }
+
+            container.BindInterfacesTo<SignalHandler<TSignal, TParameter>>().AsCached();
             return container.DeclareSignal<TSignal>();
         }
     }
