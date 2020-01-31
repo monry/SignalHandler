a8a3ba2c1af3ba833bc78151db736e8adcba54a2
Tetsuya Mori <monry84@gmail.com>
2020-01-31 12:22:26 +0900

    :sparkles: Add enum to handle cache

---
+++ Assets/Scripts/Implement/CacheType.cs
@@ -0,0 +1,30 @@
+using System;
+using UniRx;
+
+namespace SignalHandler
+{
+    public enum CacheType
+    {
+        None   = 0,
+        Latest = 1,
+        All    = 2,
+    }
+
+    internal static class CacheTypeExtension
+    {
+        internal static ISubject<TSignal> AsReplaySubject<TSignal>(this CacheType cacheType)
+        {
+            switch (cacheType)
+            {
+                case CacheType.None:
+                    return new ReplaySubject<TSignal>(0);
+                case CacheType.Latest:
+                    return new ReplaySubject<TSignal>(1);
+                case CacheType.All:
+                    return new ReplaySubject<TSignal>();
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(cacheType), cacheType, null);
+            }
+        }
+    }
+}
