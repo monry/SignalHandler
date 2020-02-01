2a118c7a6453d23c66a098ca2c52cd914b57383d
Tetsuya Mori <monry84@gmail.com>
2020-02-01 18:28:32 +0900

    :white_check_mark: Write test code
    * Debug.LogWarning を確認するために Reflection 使ってゴリ押ししている

--- Assets/Tests/Editor/Scripts/Subscriptionが無い場合に例外.cs
+++ Assets/Tests/Editor/Scripts/Subscriptionが無い場合に例外.cs
@@ -1,23 +1,43 @@
+using System.Reflection;
+using NSubstitute;
 using NUnit.Framework;
+using UnityEngine;
 using Zenject;
 
 namespace SignalHandler
 {
-    public class ThrowSignal : SignalBase<ThrowSignal>
+    public class ThrowSignal : SignalBase<ThrowSignal>, ISignalTerminator
     {
+        bool ISignalTerminator.IsTerminator { get; } = true;
     }
 
     public class Subscriptionが無い場合に例外 : ZenjectUnitTestFixture
     {
         [Inject] private ISignalPublisher<ThrowSignal> Publisher { get; }
-        [Inject] private ISignalReceiver<ThrowSignal> Receiver { get; }
+
+        private ILogger MockLogger { get; } = Substitute.For<ILogger>();
 
         [SetUp]
         public void Install()
         {
             SignalHandler<ThrowSignal>.InstallSignal(Container, signalMissingHandlerResponses: SignalMissingHandlerResponses.Throw);
 
+            // 無理矢理 UnityEngine.Debug が用いる Logger を上書き
+            typeof(Debug).GetField("s_Logger", BindingFlags.Static | BindingFlags.NonPublic)?.SetValue(null, MockLogger);
+
             Container.Inject(this);
+        }
+
+        [Test]
+        public void A_Subscriptionが無い場合に例外()
+        {
+            // この Publish により Subject が OnCompleted するので Subscription が無くなる
+            Publisher.Publish(ThrowSignal.Create());
+
+            Assert.That(() => Publisher.Publish(ThrowSignal.Create()), Throws.TypeOf<ZenjectException>());
+            Assert.That(() => Publisher.Publish(ThrowSignal.Create()), Throws.TypeOf<ZenjectException>());
+
+            MockLogger.DidNotReceive().Log(LogType.Warning, Arg.Any<string>());
         }
     }
 }
